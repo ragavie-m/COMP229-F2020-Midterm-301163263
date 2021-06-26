6cd165e16825c0d98d26e41d9914e31a107e8b86
Ragavie Mohanathas <ragaviemohanathas@gmail.com>
2021-06-26 17:59:56 -0400

    Updated db.js for MongoDB Atlas connection

--- server/config/db.js
+++ server/config/db.js
@@ -2,3 +2,7 @@
  * Add your code here to fix URI connection
  * 
  */
+module.exports = 
+{
+ "URI" : "mongodb+srv://ragavie:COMP229@mongodbserver.025wj.mongodb.net/books_list?retryWrites=true&w=majority"
+}
