8264bd7d1bba3ea7b3d170093429ce76e1f4f51a
Ragavie Mohanathas <ragaviemohanathas@gmail.com>
2021-06-26 18:19:45 -0400

    created model

--- server/models/book.js
+++ server/models/book.js
@@ -5,7 +5,17 @@ let mongoose = require('mongoose');
 /**
  * add your code to 
  * create your model here
- * 
  */
+
+let bookModel = mongoose.Schema({
+    Name:String,
+    author: String,
+    published: String,
+    description: String,
+    Price: Number
+},
+{
+    collection: "book"
+});
 
 module.exports = mongoose.model('Book', bookModel);
